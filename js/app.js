f532e113ed9cdcab66809e915c03570202c9a7bf
Michael Emefele <likemiketoo@hotmail.com>
2017-08-08 03:15:11 -0400

    Jesus this took forever, got the programming console to deny extra clicks until the animation is done

--- js/app.js
+++ js/app.js
@@ -1,37 +1,150 @@
 $(document).ready(function(){												
    //Navigation Menu Slider
-    $('#nav-expander').on('click',function(e){
+    $('#nav-expander').on('click',function(e)
+    {
         e.preventDefault();
         $('body').toggleClass('nav-expanded');
     });
-    $('#nav-close').on('click',function(e){
+    
+    $('#nav-close').on('click',function(e)
+    {
         e.preventDefault();
         $('body').removeClass('nav-expanded');
     });
 
-    // Initialize navgoco with default options
-//    $(".main-menu").navgoco({
-//        caret: '<span class="caret"></span>',
-//        accordion: false,
-//        openClass: 'open',
-//        save: true,
-//        cookie: {
-//            name: 'navgoco',
-//            expires: false,
-//            path: '/'
-//        },
-//        slide: {
-//            duration: 300,
-//            easing: 'swing'
-//        }
-//    });
-    
+    //Automatic Page scrolling
     var $root = $('html, body');
-    $('a').click(function() {
+    
+    $('a').click(function()
+    {
         $root.animate({
             scrollTop: $( $.attr(this, 'href') ).offset().top
         }, 900);
         return false;
-});
+    });
+    
+    //OMFG this next section took FOREVER to get it to behave the way I wanted it to
+    //Print Text to screen
+    var clickDisabled = false;
+    
+    var showText = function(target, message, index, interval)
+    {   
+        if(index < message.length)
+        {
+            $(target).append(message[index++]);
+            timer=setTimeout(function()
+            {
+                showText(target, message, index, interval);
+            }, interval);
+        }
+    }
+    
+    $("#htmlBtn").click(function()
+    {
+        if(clickDisabled)
+        {
+            return;
+        }
+        
+        $(exampleBox).empty();
+        showText("#exampleBox", '<div> Hello World! </div>', 0, 100);
+        
+        clickDisabled = true;
+        setTimeout(function()
+        {
+            clickDisabled = false;
+        }, 2000);
+        
+    });
+    
+    $("#cppBtn").click(function()
+    {
+        if(clickDisabled)
+        {
+            return;
+        }
+        
+        $(exampleBox).empty();
+        showText("#exampleBox", 'cout << "Hello World!";', 0, 100);
+        
+        clickDisabled = true;
+        setTimeout(function()
+        {
+            clickDisabled = false;
+        }, 2000);
+        
+    });
+    
+    $("#jsBtn").click(function()
+    {
+        if(clickDisabled)
+        {
+            return;
+        }
+        
+        $(exampleBox).empty();
+        showText("#exampleBox", 'console.log("Hello World!");', 0, 100);
+        
+        clickDisabled = true;
+        setTimeout(function()
+        {
+            clickDisabled = false;
+        }, 2000);
+        
+    });
+    
+    $("#jBtn").click(function()
+    {
+        if(clickDisabled)
+        {
+            return;
+        }
+        
+        $(exampleBox).empty();
+        showText("#exampleBox", 'system.out.println("Hello World!");', 0, 100);
+        
+        clickDisabled = true;
+        setTimeout(function()
+        {
+            clickDisabled = false;
+        }, 2000);
+        
+    });
+    
+    $("#jqBtn").click(function()
+    {
+        if(clickDisabled)
+        {
+            return;
+        }
+        
+        $(exampleBox).empty();
+        showText("#exampleBox", '$(document.body).append("Hello World!");', 0, 100);
+        
+        clickDisabled = true;
+        setTimeout(function()
+        {
+            clickDisabled = false;
+        }, 2000);
+        
+    });
+    
+    $("#cBtn").click(function()
+    {
+        if(clickDisabled)
+        {
+            return;
+        }
+        
+        $(exampleBox).empty();
+        showText("#exampleBox", 'printf("Hello World!");', 0, 100);
+        
+        clickDisabled = true;
+        setTimeout(function()
+        {
+            clickDisabled = false;
+        }, 2000);
+        
+    });
 
  });
